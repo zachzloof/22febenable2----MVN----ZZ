f0407548d0cf30f1c56c372c1478bf6096a397fc
Zach Zloof <zachzloof@hotmail.com>
2022-03-16 16:55:58 +0000

    End of day commit 16/03/2022

--- src/main/java/com/qa/jdbc/Runner.java
+++ src/main/java/com/qa/jdbc/Runner.java
@@ -1,5 +1,10 @@
 package com.qa.jdbc;
 
+import java.util.List;
+
+import com.qa.jdbc.daos.PersonDAO;
+import com.qa.jdbc.domain.Person;
+
 public class Runner {
 
 	public static void main(String[] args) {
@@ -9,6 +14,21 @@ public class Runner {
 		
 		// CRUD functionality (CREATE, READ, UPDATE, DELETE)
 		// USING DAO - DATA ACCESS OBJECT
+		
+		
+		//THIS PERSON GETS ADDED TO DATABSE CORRECTLY
+		PersonDAO pDAO = new  PersonDAO();
+//		Person p = new Person("Tom", "Smith", 30);
+//		pDAO.createPrepared(p); //AND THIS FOR PREPARED CREAte
+		
+		//THIS WILL READ BY ID
+		System.out.println(pDAO.readByID(1));
+		
+		//THIS WILL PRINT ALL / READ ALL AND PRINT LINE BY LIUNE
+		List<Person> people = pDAO.readAll();
+		for (Person person : people) {
+			System.out.println(person);
+		}
 
 	}
 
--- src/main/java/com/qa/jdbc/domain/Person.java
+++ src/main/java/com/qa/jdbc/domain/Person.java
@@ -1,5 +1,73 @@
 package com.qa.jdbc.domain;
 
 public class Person {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	
+	
+	//default constructor
+	public Person() {}
+	//constructor -- used for reading (selects)
+	public Person(int id, String firstName, String lastName, int age) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	
+	
+	
+	
+	
+	
+	// this constructor is used for creating (insert statement)
+	public Person(String firstName, String lastName, int age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+
+
+
+
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	}
+	
+	
+	
 
 }
